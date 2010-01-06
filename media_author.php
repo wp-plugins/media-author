35380b370dcd56b342dffc9175b1d409c52602dc
johnl1479 <johnl1479@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-06 06:04:15 +0000

    - Sorted user list alphabetically - Author is selected by default in the list - tagging version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/media-author/tags/1.0.1@190475 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ media_author.php
@@ -0,0 +1,85 @@
+<?php
+/*
+Plugin Name: Media Author
+Plugin URI: http://wordpress.com/extend/plugins/media-author
+Description: Allows you to change the author of a piece of media
+Plugin Author: John Luetke
+Version: 1.0.1
+Author URI: http://johnluetke.net
+*/
+
+function media_author_plugin_save ($args) {
+	
+	if (isset($_POST['post_author'])) {
+		$args['post_author'] = $_POST['post_author'];
+	}
+
+	return $args;
+}
+
+function media_author_sort($a, $b) {
+	if (!is_object($a) || !is_object($b))
+		die("Arguments must be objects");
+
+	if ($a->display_name === $b->display_name) return 0;
+
+	$vals = array($a->display_name, $b->display_name);
+	sort($vals);
+
+	if ($vals[0] === $a->display_name) return -1;
+	else return 1;
+}
+
+/*
+ * This function is the preferred method, but it relies on a fix not yet implemented. See
+ * Wordpress ticket #11705
+ */
+function media_author_plugin_dropdown ($args) {
+	$user_list = get_users_of_blog();
+	$user_array = array();
+
+	foreach ($user_list as $user) {
+		$user_array[] = array(
+			'value' => $user->user_id,
+			'label' => $user->display_name
+		);
+	}
+	
+	$users = array('post_author' => array (
+		'label' => __('Author'),
+		'input' => 'select',
+		'value' => $user_array,
+		'helps' => __('Media Author')));
+
+	return array_merge($args, $users);
+}
+
+/*
+ * This is the usable method, as of Wordpress 2.9.0
+ */
+function media_author_plugin_dropdown_2($args) {
+	$author_id = get_post($_GET['attachment_id'])->post_author;
+	
+	$user_list = get_users_of_blog();
+	usort($user_list, 'media_author_sort');
+
+	$html = "<select name='post_author' id='post_author'>";
+
+	foreach ($user_list as $user) {
+		$html .= "<option value='".$user->user_id."'".(($author_id == $user->user_id)? " selected='selected'" : "/").">".$user->display_name."</option>";
+	}
+
+	$html .= "</select>";
+	
+	$users = array('post_author' => array (
+		'label' => __('Author'),
+		'input' => 'html',
+		'html' => $html,
+		'helps' => __('Media Author')));
+
+	return array_merge($args, $users);
+}
+
+add_filter('attachment_fields_to_save', 'media_author_plugin_save', 5);
+add_filter('attachment_fields_to_edit', 'media_author_plugin_dropdown_2', 5);
+
---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== Media Author ===
+Contributors: johnl1479
+Donate link: http://johnluetke.net/donate
+Tags: media, attachment, post, author
+Requires at least: 2.8.6
+Tested up to: 2.9.1
+Stable tag: 1.0.1
+
+Allows you to change the author of a piece of media
+
+== Description ==
+
+Allows you to change the author of a piece of media by adding a drop-down list of all your blog's users to the "Edit Attachment" screen in the Media Library.
+
+== Installation ==
+
+1. Upload `media_author.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= Does this add the dropdown menu to the popup screen for media while I'm editing a post? =
+
+No, not yet.
+
+= Is there a way to limit the users shown in the dropdown? =
+
+Not yet.
+
+== Screenshots ==
+
+1. The dropdown list added the the "Edit Attachment" screen of Media Library
+
+== Changelog ==
+
+= 1.0.1 =
+* Sorted user list alphabetically
+* Author is selected by default in the list.
+
+= 1.0 =
+* Initial public version
+
+== Upgrade Notice ==
+
+No information yet
